135586b7d1d4d19a19be68b5b4b12ecb9beedc5e
suerocks <107373584+suerocks@users.noreply.github.com>
2025-06-23 15:17:40 -0400

    Update breakfast_menu.sql

--- June-2025/breakfast_menu.sql
+++ June-2025/breakfast_menu.sql
@@ -7,15 +7,15 @@ CREATE TABLE breakfast_menu (
 
 INSERT INTO breakfast_menu (customer_id, customer_name, dish_name, price)
 VALUES 
-  (001, 'Sarah', 'pancakes', 3),
+  (001, 'Sarah', 'pancakes', 4),
   (002, 'Michael', 'oatmeal', 8),
-  (003, 'Brian', 'scrambled eggs', 5),
+  (003, 'Brian', 'scrambled eggs', 3),
   (004, 'Zoro', 'yam porridge', 10),
-  (005, 'Tim', 'chocolate sundae', 7),
-  (006, 'Afia', 'greek salad', 5),
+  (005, 'Tim', 'chocolate sundae', 5),
+  (006, 'Afia', 'greek salad', 6),
   (007, 'Godwin', 'oatmeal', 8),
   (008, 'Ade', 'yam porridge', 10),
-  (009, 'Ra', 'pancakes', 3),
+  (009, 'Ra', 'pancakes', 4),
   (010, 'Sue', 'coffee', 1);
 
 SELECT * FROM breakfast_menu;
