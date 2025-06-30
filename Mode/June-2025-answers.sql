4b92c2473cc9e6ecab1ee7df01e29dea09420ec3
suerocks <107373584+suerocks@users.noreply.github.com>
2025-06-30 18:35:31 -0400

    Update June-2025-answers.sql

--- Mode/June-2025-answers.sql
+++ Mode/June-2025-answers.sql
@@ -7,3 +7,8 @@ WHERE group_name ILIKE '%ludacris%'
 
 SELECT * FROM tutorial.billboard_top_100_year_end
 WHERE group_name ILIKE 'DJ%'
+
+--3.
+
+SELECT * FROM tutorial.billboard_top_100_year_end
+WHERE group_name IN ('Elvis Presley', 'M.C. Hammer', 'Hammer')
