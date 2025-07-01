c6a380b4fb11a461c8d90484fa197b479f46e254
suerocks <107373584+suerocks@users.noreply.github.com>
2025-07-01 11:32:16 -0400

    Update July-2025-Answers.sql

--- Mode/Intermediate-SQL/July-2025-Answers.sql
+++ Mode/Intermediate-SQL/July-2025-Answers.sql
@@ -19,3 +19,13 @@ FROM tutorial.aapl_historical_stock_price
 
 SELECT SUM(open)/COUNT(open) AS avg_open_price
 FROM tutorial.aapl_historical_stock_price
+
+--4.
+
+SELECT MIN(low) AS min_stock_price
+FROM tutorial.aapl_historical_stock_price
+
+--5.
+
+SELECT MAX(close - open) AS highest_single_day_increase
+FROM tutorial.aapl_historical_stock_price
