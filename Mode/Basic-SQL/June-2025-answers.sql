eae05db7a08f634cda7ed71c732a6e22343a834c
suerocks <107373584+suerocks@users.noreply.github.com>
2025-07-06 20:33:43 -0400

    Update June-2025-answers.sql

--- Mode/Basic-SQL/June-2025-answers.sql
+++ Mode/Basic-SQL/June-2025-answers.sql
@@ -1,11 +1,11 @@
 -- 1.
 
-SELECT * FROM tutorial.billboard_top_100_year_end
+SELECT * FROM tutorial.billboard_top_100_year_end 
 WHERE group_name ILIKE '%ludacris%'
 
 --2.
 
-SELECT * FROM tutorial.billboard_top_100_year_end
+SELECT * FROM tutorial.billboard_top_100_year_end 
 WHERE group_name ILIKE 'DJ%'
 
 --3.
